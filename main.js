b7f78ccd649a8dba47139988f9e2be0ced296d8d
Mateus-Cimini <mateusmateus122333@gmail.com>
2025-06-10 14:53:07 -0300

    chore: agota calcula a media e mostra na tela

--- main.js
+++ main.js
@@ -13,32 +13,43 @@ const disciplines = [
     'Redaçao'
 ]
 
+let sum = 0;
+let median = 0;
+
 
 document.getElementById('input-discipline').value = disciplines[0];
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
+
     const inputDisciplines = document.getElementById('input-discipline')
     const inputScoreTest = document.getElementById('input-score');
-    const score = inputScoreTest.value;
+    const score = Number(inputScoreTest.value);
 
     if (scores.length >= 5) {
-    alert('Todas as 5 notas ja foram inseridas!')
-    return;
-} 
+        alert('Todas as 5 notas ja foram inseridas!');
+        return;
+    } 
 
-scores.push(score);
+    scores.push(score);
 
+    calculateFinalAverage();
+    updateFinalAverage();
 
-const newLine = `<tr>
-<td>${disciplines[scores.length - 1]}</td>
-<td>${score}</td>
-</tr>`;
+    const newLine = `<tr>
+    <td>${disciplines[scores.length - 1]}</td>
+    <td>${score}</td>
+    </tr>`;
 
     const tableBody = document.querySelector('tbody');
     tableBody.innerHTML += newLine;
     inputScoreTest.value = '';
+
+
+    const tdFinalAverage = document.getElementById('final-Average');
+    tdFinalAverage.textContent = median;
+    const finalAverage = `<td>${median}</td>`;
 
     if (scores.length < disciplines.length) {
         inputDisciplines.value = disciplines[scores.length];
@@ -47,6 +58,27 @@ const newLine = `<tr>
         inputDisciplines.value = '';
     }
 
+
+
+
 });
 
+function calculateFinalAverage() {
+     // somando resultado e salvando na variavel "sum"
+    sum = 0;
+    for (const scoreTest of scores) {
+        sum += scoreTest;
+    }
+     //salvando media do aluno e salvando na variavel "median"
+    median = sum / 5
+}
+
+
+
+function updateFinalAverage() {
+    console.log(`Nota total: ${sum}, media: ${median}`);
+    
+    
+    
+}
 
